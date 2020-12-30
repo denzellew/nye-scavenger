997cef7cd25dc1b59b4816fb2b98a2dae798675e
Denzelle Walker <dwalker@jetlifetech.com>
2020-12-30 15:33:25 -0700

    Added Real challenges

--- src/app/containers/initData.ts
+++ src/app/containers/initData.ts
@@ -2,23 +2,122 @@ import { ScavengerQuestionModel } from './HomePage/types';
 
 export const initQuestions: ScavengerQuestionModel[] = [
   {
-    location: 'This clue is found in the facet!',
-    answer: 'Some Answer',
-    answerHelp: 'Some help with this answer',
-    userAnswer: 'SomeAnswer',
-    isCorrect: false,
-  },
-  {
-    location: 'This clue is found in a wet place!',
-    answer: 'Another Answer',
-    answerHelp: 'Some help with this answer',
+    location: 'This item is found on the bathroom sink!',
+    answer: '08212017',
+    answerHelp: 'You got this...',
     userAnswer: '',
     isCorrect: false,
   },
   {
-    location: 'This clue is found in the bedroom!',
-    answer: 'Super Long Winded Answer',
-    answerHelp: 'Some help with this answer',
+    location: 'This item can be found near your fake flowers!',
+    answer: 'Moose',
+    answerHelp: 'This buddy turned out to have a similar friend',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location: 'Check our "sexy-time" chest for this item..',
+    answer: 'salsa',
+    answerHelp: 'Colombia is known for it',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location:
+      "If you head to the closet and look for ALPHA, you'll find this one",
+    answer: 'Caribbean',
+    answerHelp: 'Pirates of the.....',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location:
+      "There's a sweet guy and girl keeping this item. I mean I could seriuosly just eat them up.",
+    answer: 'Sakura',
+    answerHelp: 'The Japanese Name',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location: 'This item is brought you by USPS!',
+    answer: '05252018',
+    answerHelp: 'The Japanese Name',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location: 'Is it Nathan or Julie today?',
+    answer: 'Torre Normanna',
+    answerHelp: 'Look up Maiori on the map and go from there',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location:
+      'This next item can be found on the 6th floor commons (inside). Open some things up!',
+    answer: 'Danube',
+    answerHelp: 'Look up Budapest on the map and go from there',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location: 'Still on the 6th floor but outside... Perhaps the grill area?',
+    answer: 'La Sagrada Familia',
+    answerHelp: 'Use the full name including "the"',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location:
+      'The building has a specific room for those wanting to bike around',
+    answer: 'Dead Sea',
+    answerHelp: 'Here\'s a hint, it\'s "in" Israel',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location: 'Our favorite drawer that contains our favorite items',
+    answer: 'Gili Trawangan',
+    answerHelp: 'The full name!',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location:
+      "If you're looking for a place to meditate, this is the room for it.",
+    answer: 'Game of Thrones',
+    answerHelp: 'It was a sad highlight to our weekend',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location: "Connie's calling you, she says there's something in the trunk!",
+    answer: 'Garza Blanca',
+    answerHelp:
+      'You can find the answer at https://www.clubcaribecancun.com/resorts',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location:
+      "Oh yea! She forgot there's something in one of the compartments up front as well!",
+    answer: 'Camel',
+    answerHelp: 'You think desert animal, you think this!',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location: 'Have you checked the Christmas tree for any changes?',
+    answer: 'Marriage',
+    answerHelp: 'MARRIAGE',
+    userAnswer: '',
+    isCorrect: false,
+  },
+  {
+    location:
+      'Can you come to the 6th floor pool area? I need to ask you something',
+    answer: '',
+    answerHelp: '',
     userAnswer: '',
     isCorrect: false,
   },
