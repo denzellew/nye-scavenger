8fe479a4b06a50eef86d488751a8baa60ccca514
Denzelle Walker <dwalker@jetlifetech.com>
2020-12-30 07:20:50 -0700

    Added Components

---
+++ src/app/components/ScavengerQuestion/index.tsx
@@ -0,0 +1,20 @@
+/**
+*
+* ScavengerQuestion
+*
+*/
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+interface Props {}
+
+export function ScavengerQuestion(props: Props) {
+
+  return (
+  <Div>
+  </Div>
+  );
+
+};
+
+const Div = styled.div``;
---
+++ src/app/components/SingleInputGroup/index.tsx
@@ -0,0 +1,20 @@
+/**
+*
+* SingleInputGroup
+*
+*/
+import * as React from 'react';
+import styled from 'styled-components/macro';
+
+interface Props {}
+
+export function SingleInputGroup(props: Props) {
+
+  return (
+  <Div>
+  </Div>
+  );
+
+};
+
+const Div = styled.div``;
